7886ee21d049b09737ab11e4524b5cafbb5b4b7f
BRI <frametable@gmail.com>
2023-12-10 17:34:14 +0900

    v2

--- app.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-function handleBtnClick() {
-  console.dir(loginInput);
-  console.log("click!!!");
-}
-
-loginButton.addEventListener("click", handleBtnClick);
